931920936881a7be82c08c8c3c2b7892df90e42b
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-06-04 10:13:20 +0200

    Code added for creating new user

--- Routes/horsey-users.js
+++ Routes/horsey-users.js
@@ -37,3 +37,35 @@ router.get("/users", async (req, res) => {
 })
 
 // CREATE NEW HORSEY USER
+router.post("/users/register", async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const salt = bcrypt.genSaltSync();
+        const newUser = await new User({
+            username: username,
+            password: bcrypt.hashSync(password, salt)
+        }).save();
+// if we would like to add a link to the Users instagram for example we could add it in the response below
+// profileInstagram: newUser.profileInstagram
+        res.status(201).json({
+            success: true,
+            response: {
+                username: newUser.username,
+                id: newUser._id,
+                accessToken: newUser.accessToken,
+                profileName: newUser.profileName,
+                profileText: newUser.profileText,
+                profilePicture: newUser.profilePicture,
+                message: "User created with a loud neigh!"
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            response: {
+                error: error,
+                message: "Failed to create user"
+            }
+        })
+    }
+});
