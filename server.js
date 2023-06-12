a60ac29d069d351ad5b49064ad37c53cb34f4be4
SandraMadeleine <sandramohlsson@yahoo.com>
2023-06-12 11:25:31 +0200

    fixed untracked file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 // import crypto from "crypto";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
 import authenticateUser from './Middlewares/authentication';
 import User from './Models/user-users';
-// import Horse from './Models/
+import Horse from './Models/horse-horses';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/horsey";
+const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1/horsey';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
