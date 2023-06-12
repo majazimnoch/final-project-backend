3e7ee13c9a65df334a940f410be0b1effd57d76e
SandraMadeleine <sandramohlsson@yahoo.com>
2023-06-12 18:11:22 +0200

    additional commit before restart

--- Routes/facts-get.js
+++ Routes/facts-get.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import { facts } from '../RandomFacts/facts.json';
-const router = express.Router();
+// import express from 'express';
+// import { facts } from '../RandomFacts/facts.json';
+// const router = express.Router();
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/horsey";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+// const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/horsey";
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.Promise = Promise;
 
-router.get('/random-fact', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * facts.length);
-  const randomFact = facts[randomIndex];
-  res.json(randomFact);
-});
+// router.get('/random-fact', (req, res) => {
+//   const randomIndex = Math.floor(Math.random() * facts.length);
+//   const randomFact = facts[randomIndex];
+//   res.json(randomFact);
+// });
 
-export default router;
+// export default router;
